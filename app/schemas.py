f1a4ea124a5051ccd7042305aef618364473250d
PaulOchoa952 <paalochoa@ittepic.edu.mx>
2025-04-30 11:03:20 -0700

    add comments on schemans

--- app/schemas.py
+++ app/schemas.py
@@ -5,11 +5,11 @@ class MovieBase(BaseModel):
     name: str
     release_date: str
 
-
+# This is the MovieCreate model that represents the data for creating a new movie.
 class MovieCreate(MovieBase):
     pass
 
-
+#this is the Movie model that represents the movies table in the database.
 class Movie(BaseModel):
     id: int
     name: str
@@ -18,8 +18,9 @@ class Movie(BaseModel):
     status: str
 
     class Config:
-        from_attributes = True  # Replace orm_mode with from_attributes
+        from_attributes = True # Enable ORM mode for Pydantic models
 
+# This is the Log model that represents the logs table in the database.
 class LogResponse(BaseModel):
     method: str
     endpoint: str
