41b6b423cbb8d05f27fae5b8725392169d3777e0
PaulOchoa952 <paalochoa@ittepic.edu.mx>
2025-04-30 12:35:49 -0700

    add env files

--- app/database.py
+++ app/database.py
@@ -1,13 +1,24 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from dotenv import load_dotenv
+import os
+
+#load environment variables from .env file
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    print("dotenv module not found. Skipping loading environment variables.")
+    raise
 
 # Database URL points to SQLite
-DATABASE_URL = "sqlite:///./movies.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./movies.db")
 
 # Create SQLAlchemy engine and sessionmaker
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 
 # Initialize the database
